a39f3beb086a8b4b515d7088bad0790bcbbcb6c1
Snow <a.dolin125@gmail.com>
2020-11-27 17:04:52 +0200

    new week start

---
+++ Structures (date, week 4).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using std::map;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+
+
+
+
+
+    return 0;
+}
